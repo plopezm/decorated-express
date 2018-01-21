52d48fa2f8e8924cbffaf6158ad476e928f2c6ac
Pablo LM <pabloplm@gmail.com>
2018-01-21 16:05:30 +0100

    refactoring

--- src/security/bearer/jwt.decorator.ts
+++ src/security/bearer/jwt.decorator.ts
@@ -2,6 +2,74 @@ import * as express from 'express';
 import * as jwtlib from "jsonwebtoken";
 import { SignCallback } from 'jsonwebtoken';
 
+export function JWTAuth(cert: string, options?: JWTSignOptions) {
+    return function (target: Object, key: string, descriptor: TypedPropertyDescriptor<any>){
+        if(!descriptor.value.middlewares){
+            descriptor.value.middlewares = [];
+        }
+        JWTAuthenticationMiddleware = JWTAuthenticationMiddleware.bind(descriptor.value);
+        //descriptor.value.validateJwtCredentials = validateJwtCredentials;
+        descriptor.value.jwt = {options: options};
+        descriptor.value.jwt = {cert: cert};
+        descriptor.value.middlewares.push(JWTAuthenticationMiddleware);
+        return descriptor;
+    }
+}
+
+var JWTAuthenticationMiddleware = function(req: express.Request, res: express.Response, next: Function) { 
+    const authpreffix = "Bearer ";
+    //Parses request and calls validation method
+    let jwtToken = req.headers[HEADER_AUTHENTICATION];
+    if(!jwtToken || typeof jwtToken !== "string"){
+        sendNotAllowed(res, `[JWTAuth]: ${HEADER_AUTHENTICATION} header not found`);
+        return;
+    }
+    let encodedPartIndex = jwtToken.indexOf(authpreffix);
+    if(encodedPartIndex === -1){
+        sendNotAllowed(res, `[JWTAuth]: ${HEADER_AUTHENTICATION} type not valid '${jwtToken}'`);
+        return;
+    }
+    encodedPartIndex += authpreffix.length;
+    //Getting string token
+    jwtToken = jwtToken.substr(encodedPartIndex)
+
+    jwtlib.verify(jwtToken, this.jwt.cert, this.jwt.options, function(err: any, decoded: any) {      
+        if (err) {
+            sendNotAllowed(res, err);
+            return
+        } else {
+          storeCredentails(req, decoded);
+          next();
+        }
+    });
+}
+
+export class JWTFactory {
+    static generateToken(secret: string, payload: any, options: JWTSignOptions = {expiresIn: 1440}): any {        
+        var token = jwtlib.sign(payload, secret, options);
+        return token;
+    }
+}
+
+function sendNotAllowed(res: express.Response, message: string){
+    res.status(401);
+    res.json({status: 401, msg: message});
+}
+
+function storeCredentails(req: express.Request, payload: any) {
+    if(!req.params.auth){
+        req.params.auth = {
+            jwt: {
+                payload: payload
+            }
+        }
+    }else{
+        req.params.auth.jwt = {
+            payload: payload
+        }
+    }
+}
+
 export interface JWTSignOptions {
     /**
      * Signature algorithm. Could be one of these values :
@@ -34,71 +102,3 @@ export interface JWTSignOptions {
 export const HEADER_AUTHENTICATION: string = "authorization";
 
 export type JWTCredentialsValidator = (jwt: any) => boolean;
-
-export function JWTAuth(cert: string, options?: JWTSignOptions) {
-    return function (target: Object, key: string, descriptor: TypedPropertyDescriptor<any>){
-        if(!descriptor.value.middlewares){
-            descriptor.value.middlewares = [];
-        }
-        JWTAuthenticationMiddleware = JWTAuthenticationMiddleware.bind(descriptor.value);
-        //descriptor.value.validateJwtCredentials = validateJwtCredentials;
-        descriptor.value.jwtoptions = options;
-        descriptor.value.cert = cert;
-        descriptor.value.middlewares.push(JWTAuthenticationMiddleware);
-        return descriptor;
-    }
-}
-
-function sendNotAllowed(res: express.Response, message: string){
-    res.status(401);
-    res.json({status: 401, msg: message});
-}
-
-function storeCredentails(req: express.Request, payload: any) {
-    if(!req.params.auth){
-        req.params.auth = {
-            jwt: {
-                payload: payload
-            }
-        }
-    }else{
-        req.params.auth.jwt = {
-            payload: payload
-        }
-    }
-}
-
-var JWTAuthenticationMiddleware = function(req: express.Request, res: express.Response, next: Function) { 
-    const authpreffix = "Bearer ";
-    //Parses request and calls validation method
-    let jwtToken = req.headers[HEADER_AUTHENTICATION];
-    if(!jwtToken || typeof jwtToken !== "string"){
-        sendNotAllowed(res, `[JWTAuth]: ${HEADER_AUTHENTICATION} header not found`);
-        return;
-    }
-    let encodedPartIndex = jwtToken.indexOf(authpreffix);
-    if(encodedPartIndex === -1){
-        sendNotAllowed(res, `[JWTAuth]: ${HEADER_AUTHENTICATION} type not valid '${jwtToken}'`);
-        return;
-    }
-    encodedPartIndex += authpreffix.length;
-    //Getting string token
-    jwtToken = jwtToken.substr(encodedPartIndex)
-
-    jwtlib.verify(jwtToken, this.cert, this.jwtoptions, function(err: any, decoded: any) {      
-        if (err) {
-            sendNotAllowed(res, err);
-            return
-        } else {
-          storeCredentails(req, decoded);
-          next();
-        }
-    });
-}
-
-export class JWTFactory {
-    static generateToken(secret: string, payload: any, options: JWTSignOptions = {expiresIn: 1440}): any {        
-        var token = jwtlib.sign(payload, secret, options);
-        return token;
-    }
-}
