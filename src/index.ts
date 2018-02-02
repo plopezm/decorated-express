3624935402f3f9893755ce1bd808b02fbb7fec0c
Pablo LM <pabloplm@gmail.com>
2018-02-03 00:22:49 +0100

    Documentation improved

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { Server } from './core/server';
 export { GET, POST, PUT, DELETE, Middlewares } from './decorations/resource.decoration';
 export { AuthenticationData, BasicData, JWTData } from './security/security.interfaces';
 export { BasicAuth } from './security/basic/authentication.decorator';
-export { JWTAuth, JWTFactory, JWTSignOptions } from './security/bearer/jwt.decorator';
+export { JWTAuth, JWTFactory, VerifyOptions, SignOptions } from './security/bearer/jwt.decorator';
--- src/security/bearer/jwt.decorator.ts
+++ src/security/bearer/jwt.decorator.ts
@@ -1,11 +1,12 @@
 import * as express from 'express';
 import * as jwtlib from "jsonwebtoken";
-import { SignCallback } from 'jsonwebtoken';
+import { SignCallback, VerifyOptions, SignOptions } from 'jsonwebtoken';
+export { VerifyOptions, SignOptions } from 'jsonwebtoken';
 import { JWTData } from '../security.interfaces';
 
 export const HEADER_AUTHENTICATION: string = "authorization";
 
-export function JWTAuth(cert: string, options?: JWTSignOptions) {
+export function JWTAuth(cert: string, options?: VerifyOptions) {
     return function (target: Object, key: string, descriptor: TypedPropertyDescriptor<any>){
         if(!descriptor.value.middlewares){
             descriptor.value.middlewares = [];
@@ -46,13 +47,6 @@ var JWTAuthenticationMiddleware = function(req: express.Request, res: express.Re
     });
 }
 
-export class JWTFactory {
-    static generateToken(secret: string, payload: any, options: JWTSignOptions = {expiresIn: 1440}): any {        
-        var token = jwtlib.sign(payload, secret, options);
-        return token;
-    }
-}
-
 function sendNotAllowed(res: express.Response, message: string){
     //console.log(`[JWT] Request not allowed, message: ${message}`);
     res.status(401);
@@ -72,31 +66,9 @@ function storeCredentails(res: express.Response, payload: any) {
     res.locals.auth.jwt = jwtData;
 }
 
-export interface JWTSignOptions {
-    /**
-     * Signature algorithm. Could be one of these values :
-     * - HS256:    HMAC using SHA-256 hash algorithm (default)
-     * - HS384:    HMAC using SHA-384 hash algorithm
-     * - HS512:    HMAC using SHA-512 hash algorithm
-     * - RS256:    RSASSA using SHA-256 hash algorithm
-     * - RS384:    RSASSA using SHA-384 hash algorithm
-     * - RS512:    RSASSA using SHA-512 hash algorithm
-     * - ES256:    ECDSA using P-256 curve and SHA-256 hash algorithm
-     * - ES384:    ECDSA using P-384 curve and SHA-384 hash algorithm
-     * - ES512:    ECDSA using P-521 curve and SHA-512 hash algorithm
-     * - none:     No digital signature or MAC value included
-     */
-    algorithm?: string;
-    keyid?: string;
-    /** @member {string} - expressed in seconds or a string describing a time span [zeit/ms](https://github.com/zeit/ms.js).  Eg: 60, "2 days", "10h", "7d" */
-    expiresIn?: string | number;
-    /** @member {string} - expressed in seconds or a string describing a time span [zeit/ms](https://github.com/zeit/ms.js).  Eg: 60, "2 days", "10h", "7d" */
-    notBefore?: string | number;
-    audience?: string | string[];
-    subject?: string;
-    issuer?: string;
-    jwtid?: string;
-    noTimestamp?: boolean;
-    header?: object;
-    encoding?: string;
+export class JWTFactory {
+    static generateToken(secret: string, payload: any, options: SignOptions = {expiresIn: 1440}): any {        
+        var token = jwtlib.sign(payload, secret, options);
+        return token;
+    }
 }
