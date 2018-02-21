72aeea022e89625f147085d356da92855e07e443
plopezm <pabloplm@gmail.com>
2018-02-21 17:45:16 +0100

    Implemented routepath for resources classes

--- src/core/server.ts
+++ src/core/server.ts
@@ -19,6 +19,11 @@ export class Server {
 
     registerResource(clazz: any) {        
         let resource = Object.create(clazz.prototype);
+        let routePath = resource.routePath;
+        if (!routePath) {
+            routePath = '';
+        }
+        delete clazz.prototype.routePath;
         Object.getOwnPropertyNames(clazz.prototype).forEach((key: string) => {
             if (clazz.prototype[key] === undefined || !(clazz.prototype[key] instanceof Function)){
                 return;
@@ -26,7 +31,7 @@ export class Server {
             let resourcePath = clazz.prototype[key].path;
             let resourceMethod = clazz.prototype[key].method;
             let middlewares = clazz.prototype[key].middlewares;
-            delete clazz.prototype[key].path
+            delete clazz.prototype[key].path;
             delete clazz.prototype[key].method;
             delete clazz.prototype[key].middlwares;
             if (!resourcePath || !resourceMethod){
@@ -37,8 +42,8 @@ export class Server {
             }
             let resourceFunction: Function = clazz.prototype[key];
             resourceFunction = resourceFunction.bind(resource);
-            this.router[resourceMethod](resourcePath, middlewares, resourceFunction);
-            console.log("[decorated-express]: function " + key + " added in path '" + resourcePath + "' with method '"+resourceMethod+"'");            
+            this.router[resourceMethod](routePath + resourcePath, middlewares, resourceFunction);
+            console.log("[decorated-express]: function " + key + " added in path '" + routePath + resourcePath + "' with method '"+resourceMethod+"'");            
         });
     }
 
